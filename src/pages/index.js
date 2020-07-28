c3dee15d43d30136ce096d1e8e02175a8f560558
Zach Tapia <zachtapia@gmail.com>
2020-07-28 11:50:18 -0700

    completed mission section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
 import Layout from "../components/Layout/Layout"
+import HomeLanding from "../components/Home/HomeLanding/HomeLanding"
+import HomeMission from "../components/Home/HomeMission/HomeMission"
 
 const Index = () => (
   <Layout>
-    <h1>Home</h1>
+    <HomeLanding />
+    <HomeMission />
   </Layout>
 )
 
