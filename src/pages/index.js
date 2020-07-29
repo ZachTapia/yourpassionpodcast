d3e5dcf699ec6899a69125800d3028fcc8bf2c14
Zach Tapia <zachtapia@gmail.com>
2020-07-28 17:12:38 -0700

    created guest list

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,12 @@ import React from "react"
 import Layout from "../components/Layout/Layout"
 import HomeLanding from "../components/Home/HomeLanding/HomeLanding"
 import HomeMission from "../components/Home/HomeMission/HomeMission"
-
+import HomeGuests from "../components/Home/HomeGuests/HomeGuests"
 const Index = () => (
   <Layout>
     <HomeLanding />
     <HomeMission />
+    <HomeGuests />
   </Layout>
 )
 
